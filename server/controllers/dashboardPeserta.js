d61706c37204034aa88d12362e4c904753814488
Mahesa Alamsyah <mahesanuralamsyah@gmail.com>
2023-06-13 21:06:59 +0700

    add ednpoint dashboard peserta

--- server/controllers/dashboardPeserta.js
+++ server/controllers/dashboardPeserta.js
@@ -1,23 +1,22 @@
-const { Kelas, Kelas_Peserta, Karya, Karya_Peserta, Berita } = require("../models");
-const { verifyToken } = require("../middlewares/jwt");
+const {Kelas, Karya, Kelas_Peserta, Karya_Peserta } = require('../models')
+const { verifyToken } = require('../middlewares/jwt')
 
-class AfterLogin {
-	static async generateInfoPeserta(req, res, next) {
-		try {
+class DashboardUser {
+    static async kelas(req,res) {
+        try {
             const dataToken = await verifyToken(req.headers.token, process.env.SECRET_KEY)
 			const dataKelasPeserta = await Kelas_Peserta.findAll({where: {id_peserta: dataToken.id}})
             const kelasId = dataKelasPeserta.map(data => data.id_kelas)
             const kelas = await Kelas.findAll({where: {id: [kelasId]}})
             
-            let infoPeserta = {
+            let dashboard = {
                 kelas_peserta: kelas
             }
-			req.infoPeserta = infoPeserta
-            next()
+            res.status(200).send(dashboard)
 		} catch {
             res.status(500).send({msg: "Internal Server Error"})
-        }
-	}
+        }     
+    }
 }
 
-module.exports = AfterLogin;
+module.exports=DashboardUser
